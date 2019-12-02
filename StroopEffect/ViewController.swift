b663b9740117fbc1b11a57a93b81fc01f27e8d95
Cao Mai <caomai@rocketmail.com>
2019-12-01 19:49:07 -0800

    Initial Commit

---
+++ StroopEffect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StroopEffect
+//
+//  Created by Cao Mai on 12/1/19.
+//  Copyright © 2019 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
